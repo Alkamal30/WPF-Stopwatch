44fdf5773c9b1c5bd666fec7301ef0b795ae4ece
Alkamal <yurevich.vlad3005@gmail.com>
2021-07-27 20:43:03 +0300

    Добавьте файлы проекта.

---
+++ WPF Stopwatch/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Stopwatch {
+    public partial class MainWindow : Window {
+
+        private Stopwatch _stopwatch;
+        private string _outputFormat = @"hh\:mm\:ss\.fff";
+
+
+        public MainWindow() {
+            InitializeComponent();
+
+            _stopwatch = new Stopwatch();
+
+            BindButtonsReactions();
+            LaunchUpdateTimer();
+        }
+
+        private void BindButtonsReactions() {
+            StartButton.Click += OnStartButtonClicked;
+            StopButton.Click += OnStopButtonClicked;
+            RestartButton.Click += OnRestartButtonClicked;
+        }
+
+        private void OnStartButtonClicked(object sender, RoutedEventArgs e) {
+            _stopwatch.Start();
+        }
+        
+        private void OnStopButtonClicked(object sender, RoutedEventArgs e) {
+            _stopwatch.Stop();
+        }
+
+        private void OnRestartButtonClicked(object sender, RoutedEventArgs e) {
+            _stopwatch.Restart();
+        }
+
+
+        private void LaunchUpdateTimer() {
+            System.Windows.Threading.DispatcherTimer updateTimer = new System.Windows.Threading.DispatcherTimer();
+            updateTimer.Tick += new EventHandler(UpdateTimerTick);
+            updateTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            updateTimer.Start();
+        }
+
+        private void UpdateTimerTick(object sender, EventArgs eventArgs) {
+            StopwatchText.Text = _stopwatch.DeltaTime.ToString(_outputFormat);
+        }
+    }
+}
---
+++ WPF Stopwatch/Stopwatch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Stopwatch {
+    class Stopwatch {
+
+        private bool _isWorked;
+        private DateTime _startTime;
+        private TimeSpan _deltaTime;
+
+        public TimeSpan DeltaTime {
+            get {
+                if(_isWorked)
+                    return _deltaTime + CalculateDifferenceBetweenStartTimeAndCurrentTime();
+                else
+                    return _deltaTime;
+            }
+        }
+
+
+        public Stopwatch() {
+            _isWorked = false;
+            NullifyVariables();
+        }
+
+        public void Start() {
+            if(!_isWorked) {
+                StartWork();
+            }
+        }
+
+        public void Stop() {
+            if(_isWorked) {
+                StopWork();
+            }
+        }
+
+        public void Restart() {
+            Stop();
+            NullifyVariables();
+        }
+
+
+        private void NullifyVariables() {
+            _startTime = new DateTime();
+            _deltaTime = new TimeSpan();
+        }
+
+        private void StartWork() {
+            _isWorked = true;
+            _startTime = DateTime.Now;
+        }
+
+        private void StopWork() {
+            _isWorked = false;
+            _deltaTime += CalculateDifferenceBetweenStartTimeAndCurrentTime();
+        }
+
+        private TimeSpan CalculateDifferenceBetweenStartTimeAndCurrentTime() {
+            DateTime currentTime = DateTime.Now;
+            return currentTime.Subtract(_startTime);
+        }
+
+    }
+}
